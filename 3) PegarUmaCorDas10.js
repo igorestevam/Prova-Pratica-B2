01b8d0da759a9be8bee7a52fcfd89f30b648ebe3
Igor Estevam <igorestevam1900@gmail.com>
2024-11-26 07:57:13 -0300

    Update 3) PegarUmaCorDas10.js

--- 3) PegarUmaCorDas10.js
+++ 3) PegarUmaCorDas10.js
@@ -1,5 +1,5 @@
 function getRandomColorFrom10() {
-    const randomColors = getRandomColors(); // Chama a função getRandomColors para obter as 10 cores aleatórias
-    const randomIndex = Math.floor(Math.random() * randomColors.length); // Seleciona um índice aleatório entre as 10 cores
-    return randomColors[randomIndex]; // Retorna a cor aleatória
+    const randomColors = getRandomColors();
+    const randomIndex = Math.floor(Math.random() * randomColors.length);
+    return randomColors[randomIndex];
 }
